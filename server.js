c53e1c6870e40731a950dec8a56e30422542c68e
simon.du <a932455223@qq.com>
2018-02-05 21:31:02 +0800

    filterBox  opened

--- server.js
+++ server.js
@@ -3,33 +3,41 @@ const send = require('koa-send')
 const path = require('path')
 const sqlite3 = require('co-sqlite3')
 const router = require('koa-router')()
+const logger = require('koa-logger')
 const app = new Koa()
 const port = 3000
 const webpack = require('webpack')
 const webpackDevMiddleware = require('koa-webpack-dev-middleware')
 let webpackConfig = require('./webpack.config.js')
+app.use(logger())
+app.use(async (ctx,next) => {
+	if(ctx.method === 'GET' && ['/','/login','/customer/my','/branch/list','/customer/focus'].includes(ctx.path)){
+		ctx.path = '/'
+	}
+	await next();
+})
 
-// app.use(async (ctx,next) => {
-// 	if(ctx.method === 'GET' && ['/','/login','/customer/my','/branch/list','/customer/focus'].includes(ctx.path)){
-// 		ctx.path = '/'
-// 	}
-// 	await next();
-// })
-
-// app.use(webpackDevMiddleware(webpack(webpackConfig),{
-// 	stats:{
-// 		colors:true
-// 	}
-// }))
+app.use(webpackDevMiddleware(webpack(webpackConfig),{
+	stats:{
+		colors:true
+	}
+}))
 
 router.get('/',ctx => {
 		ctx.body = ctx.webpack.fileSystem.readFileSync('./dist/index.html')
 })
 
+// sqlite3('./src/DataBase/crm.db').then((db)=>{
+// 	return db.prepare('insert into user(username,password,createTime) values(?,?,?)',['tom','123456',Date().toString()])
+// }).then((stmt)=>{
+// 	stmt.run();
+// 	console.log('data insert sucess')
+// })
+
 router.get('/user',async (ctx,next) => {
-	console.log('/user log')
 	const db = await sqlite3('./src/DataBase/crm.db');
-	ctx.body = await db.get('select * from user')
+	let data = await db.all('select * from user;')
+	ctx.body = data
 })
 app.use(router.routes())
 app.use(router.allowedMethods())
--- src/Pages/components/DataTable/index.js
+++ src/Pages/components/DataTable/index.js
@@ -1,20 +1,22 @@
 import React,{Component} from 'react'
 import {Input,Button,Tag,Table} from 'antd'
+import  classNames  from 'classnames'
 const Search = Input.Search;
-
+const { CheckableTag } = Tag
 const filterData = {
-	customer:{
-		filter:[{
-			filterName:'客户类别',
-			paramName:'customerCategory'
-		},{
-			filterName:'评级',
-			paramName:'lever'
-		},{
-			filterName:'风险偏好',
-			paramName:'riskPreference'
-		}]
-	}
+	customer:[{
+		filterText:'客户类别',
+		paramName:'customerCategory',
+		options:[{id:1,text:'个人客户'},{id:2,text:'企业客户'}]
+	},{
+		filterText:'评级',
+		paramName:'lever',
+		options:[{id:3,text:'普通客户'},{id:4,text:'VIP客户'},{id:5,text:'未激活客户'}]
+	},{
+		filterText:'风险偏好',
+		paramName:'riskPreference',
+		options:[{id:6,text:'保守型'},{id:7,text:'激进型'},{id:8,text:'稳健性'}]
+	}]
 }
 const dataSource = [{
   key: '1',
@@ -67,17 +69,27 @@ const paginationConfig = {
 export default class DataTable extends Component{
 
 	state = {
-		selectedRowKeys:[]
+		selectedRowKeys:[],
+		selectedFilter:[],
+		openFilter:false
 	}
 
 	onSelectChange = ()=>{
-
 	}
 
 	onTableChange = (pagination,filter,sorter)=>{
 		console.log(pagination)
 		console.log(filter)
 		console.log(sorter)
+	}
+
+	onSelect(index) {
+		let paramName = filterData.customer[index].paramName
+	}
+	openFilter = () => {
+		this.setState({
+			openFilter:!this.state.openFilter
+		})
 	}
 	render(){
 		const rowSelection = {
@@ -89,13 +101,20 @@ export default class DataTable extends Component{
 				<div className="topBox">
 					<div className="customerList">客户列表</div>
 					<div className="filterHandler">
-						高级筛选
-						<div className="filterbox">
-							<div>
-								<h4 className="title"><span>.</span>客户类型</h4>
-								<Tag>个人客户</Tag>
-								<Tag>企业客户</Tag>
-							</div>
+						<span onClick={this.openFilter}>高级筛选</span>
+						<div className={classNames('filterbox',{active:this.state.openFilter})}>
+						{
+							filterData.customer.map((fliter,index) =>{
+								return (
+								<div key={fliter.paramName}>
+									<h4 className="title">{fliter.filterText}</h4>
+									{fliter.options.map((option)=> {
+										return <CheckableTag key={option.id.toString()} onChange={this.onSelect.bind(this,index)}>{option.text}</CheckableTag>
+									})}
+								</div>)
+							})
+						}
+
 						</div>
 					</div>
 					<div className="searchBox">
@@ -112,7 +131,10 @@ export default class DataTable extends Component{
 						<span className="category">客户类别：</span><Tag  closable>个人客户</Tag><Tag  closable>个人客户</Tag><Tag  closable>个人客户</Tag>
 					</div>
 					<div className="filterItem">
-						<span className="category">客户类别：</span><Tag  closable>个人客户</Tag><Tag  closable>个人客户</Tag><Tag  closable>个人客户</Tag>
+						<span className="category">评级：</span><Tag  closable>个人客户</Tag><Tag  closable>个人客户</Tag><Tag  closable>个人客户</Tag>
+					</div>
+					<div className="filterItem">
+						<span className="category">风险偏好：</span><Tag  closable>个人客户</Tag><Tag  closable>个人客户</Tag><Tag  closable>个人客户</Tag>
 					</div>
 				</div>
 				<Table className="yptable" onChange={this.onTableChange} rowSelection={rowSelection} dataSource={dataSource} columns={columns} pagination={paginationConfig} />
